e523939ccf809affeb0f87c1eefe1124de9df598
Lika <chumachenko2005@icloud.com>
2024-04-07 15:30:23 +0300

    int strcmp

--- string/tasks/string_.c
+++ string/tasks/string_.c
@@ -50,5 +50,9 @@ char* findSpaceReverse(char *rbegin, const char *rend){
 }
 
 int strcmp(const char *lhs, const char *rhs) {
-
+    while (*lhs != '\0' && *lhs == *rhs) {
+        lhs++;
+        rhs++;
+    }
+    return *lhs - *rhs;
 }
